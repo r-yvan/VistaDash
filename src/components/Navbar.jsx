795414c9ea52249f74a22dce4e9632c2a156c77f
r-yvan <yvankiliye.rubuto@gmail.com>
2024-09-15 09:15:45 +0200

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,37 @@ const Navbar = () => {
       <div></div>
       <div>
         <p>Main Menu</p>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <div></div>
+          <div><p>Home</p></div>
+        </div>
+        <div>
+          <div></div>
+          <div><p>Dashboard</p></div>
+        </div>
+        <div>
+          <div></div>
+          <div><p>Products</p></div>
+        </div>
+        <div>
+          <div></div>
+          <div><p>Invoice</p></div>
+        </div>
       </div>
       <div>
         <p>Help & Support</p>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <div></div>
+          <div><p>Settings</p></div>
+        </div>
+        <div>
+          <div></div>
+          <div><p>Help & Center</p></div>
+        </div>
+        <div>
+          <div></div>
+          <div><p>LogOut</p></div>
+        </div>
       </div>
     </div>
   );
